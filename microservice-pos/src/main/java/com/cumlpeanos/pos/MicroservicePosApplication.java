6528946babd9137fd8fe05e8791b05c01d250764
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-07-10 15:59:43 -0500

    feat: (microservice POS)

---
+++ microservice-pos/src/main/java/com/cumlpeanos/pos/MicroservicePosApplication.java
@@ -0,0 +1,13 @@
+package com.cumlpeanos.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicePosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicePosApplication.class, args);
+	}
+
+}
