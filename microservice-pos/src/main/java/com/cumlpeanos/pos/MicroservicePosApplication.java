35259807678fe0ac82a7fd9fec6df5b22ee8ec8f
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-07-11 14:22:02 -0500

    feat:(swagger io )

--- microservice-pos/src/main/java/com/cumlpeanos/pos/MicroservicePosApplication.java
+++ microservice-pos/src/main/java/com/cumlpeanos/pos/MicroservicePosApplication.java
@@ -1,11 +1,14 @@
 package com.cumlpeanos.pos;
 
+import com.cumlpeanos.pos.config.OpenAPIConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Import(OpenAPIConfiguration.class)
 public class MicroservicePosApplication {
 
 	public static void main(String[] args) {
