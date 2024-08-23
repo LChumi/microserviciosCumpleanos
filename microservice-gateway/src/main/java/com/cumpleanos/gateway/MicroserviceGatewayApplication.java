e6a5c60e9472cbdafc1fa438059d293950b24d3b
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-08-23 17:14:45 -0500

    feat(adding swagger to gateway)

--- microservice-gateway/src/main/java/com/cumpleanos/gateway/MicroserviceGatewayApplication.java
+++ microservice-gateway/src/main/java/com/cumpleanos/gateway/MicroserviceGatewayApplication.java
@@ -1,8 +1,19 @@
 package com.cumpleanos.gateway;
 
+import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties.SwaggerUrl;
+import org.springdoc.core.properties.SwaggerUiConfigParameters;
+import org.springdoc.core.utils.Constants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -12,4 +23,20 @@ public class MicroserviceGatewayApplication {
 		SpringApplication.run(MicroserviceGatewayApplication.class, args);
 	}
 
+	@Bean
+	@Lazy(false)
+	public Set<SwaggerUrl> apis(RouteDefinitionLocator locator, SwaggerUiConfigParameters swaggerUiConfigParameters) {
+		Set<SwaggerUrl> urls = new HashSet<>();
+		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+		if (definitions != null) {
+			definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
+				String name = routeDefinition.getId().replaceAll("-service", "");
+				SwaggerUrl swaggerUrl = new SwaggerUrl(name, "/" + name + "/v3/api-docs", null);
+				System.out.println(swaggerUrl);  // Asegúrate de que esta línea está imprimiendo URLs correctas
+				urls.add(swaggerUrl);
+			});
+		}
+		swaggerUiConfigParameters.setUrls(urls);
+		return urls;
+	}
 }
