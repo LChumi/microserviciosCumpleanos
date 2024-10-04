4dc57f87adca71ff0a5a7d846ebf87eeff43e6ff
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-10-04 18:33:39 -0500

    feat(creation enums to paramets)

---
+++ microservice-reccomprobantes/src/main/java/com/cumpleanos/reccomprobantes/models/enums/ParametCoaEnum.java
@@ -0,0 +1,27 @@
+package com.cumpleanos.reccomprobantes.models.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ParametCoaEnum {
+    COA("COM", "COA", "CODIGO DE TABLA COA, EL CUAL VA A VALIDAR LOS TIPOS DE COMPROBANTE AL REGISTRAR EL DETALLE DE GASTOS", 3),
+    POL("COM", "SEC", "CODIGO DE LA TABLA DEL COA QUE TIENE LA SECUENCIA DE TRANSACCION", 2),
+    CRE("COM", "CRE", "CODIGO DE LA TABLA DEL COA QUE TIENE EL CREDITO TRIBUTARIO", 4),
+    DEV("COM","DEV", "CODIGO DE LA TABLA DEL COA QUE TIENE EL DERECHO A DEVOLUCION", 9);
+
+    private final String sigla;
+    private final String secuencia;
+    private final String nombre;
+    private final int valor;
+
+    public static ParametCoaEnum getBySigla(String sigla) {
+        for (ParametCoaEnum parametro : ParametCoaEnum.values()) {
+            if (parametro.getSigla().equals(sigla)) {
+                return parametro;
+            }
+        }
+        return null;
+    }
+}
