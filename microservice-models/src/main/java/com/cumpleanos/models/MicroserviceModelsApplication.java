90d97dc2f9383be11369e88882646d7454229001
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-10-04 10:10:40 -0500

    feat(adding swagger)
    change dependency to sagger and adi openapidefinition

--- microservice-models/src/main/java/com/cumpleanos/models/MicroserviceModelsApplication.java
+++ microservice-models/src/main/java/com/cumpleanos/models/MicroserviceModelsApplication.java
@@ -1,5 +1,7 @@
 package com.cumpleanos.models;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -8,6 +10,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 @EntityScan(basePackages = "core.cumpleanos")
+@OpenAPIDefinition(info =
+@Info(title = "API Modelos ", description = "Documentacion API Modelos v1.0")
+)
 public class MicroserviceModelsApplication {
 
 	public static void main(String[] args) {
