12440ff3af1d0ec8a43971ddbf869c847a447b82
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-10-22 18:04:44 -0500

    feat(Configure email settings for Spring application)

---
+++ microservice-notification/src/main/java/com/cumpleanos/notification/configuration/EmailConfig.java
@@ -0,0 +1,41 @@
+package com.cumpleanos.notification.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:email.yml")
+public class EmailConfig {
+
+    @Value("${email.sender}")
+    private String emailUser;
+
+    @Value(("${email.password}"))
+    private String emailPassword;
+
+    @Bean
+    public JavaMailSender getMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(emailUser);
+        mailSender.setPassword(emailPassword);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        props.put("mail.smtp.connectiontimeout", "10000");
+        props.put("mail.smtp.timeout", "10000");
+        return mailSender;
+    }
+}
