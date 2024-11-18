252103ec768503949dc7f36e712e36bf5356ba64
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-11-18 11:05:16 -0500

    feat:(Add new module with Swagger integration and configuration)
    -Added a new module to the microservice
    
    -Integrated Swagger for API documentation
    
    Update configuration files to include module setup

---
+++ microservice-assist/src/main/java/com/cumpleanos/assist/AssistServiceApplication.java
@@ -0,0 +1,19 @@
+package com.cumpleanos.assist;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@OpenAPIDefinition( info =
+@Info(title = "ASSIST WEB", description = "Documentacion ASSIST WEB API v 1"))
+public class AssistServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistServiceApplication.class, args);
+	}
+
+}
---
+++ microservice-assist/src/test/java/com/cumpleanos/assist/AssistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cumpleanos.assist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
