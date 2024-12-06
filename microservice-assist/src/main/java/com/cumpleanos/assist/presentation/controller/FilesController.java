61868feff6e6f5fcfc775d5b6dbaec81ef8b8936
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-12-06 12:05:24 -0500

    fix(change endpoint in  @controller to @RestController  )

--- microservice-assist/src/main/java/com/cumpleanos/assist/presentation/controller/FilesController.java
+++ microservice-assist/src/main/java/com/cumpleanos/assist/presentation/controller/FilesController.java
@@ -6,16 +6,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-@Controller
-@RestController("assist")
+@RestController
+@RequestMapping("assist")
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @Slf4j
 public class FilesController {
