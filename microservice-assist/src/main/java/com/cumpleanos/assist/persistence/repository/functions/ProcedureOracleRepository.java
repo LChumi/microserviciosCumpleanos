57475d6ca6c2af1e56e569b5f6ea98562b371e47
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-01-16 18:32:51 -0500

    fix: changes in procedure repository

--- microservice-assist/src/main/java/com/cumpleanos/assist/persistence/repository/functions/ProcedureOracleRepository.java
+++ microservice-assist/src/main/java/com/cumpleanos/assist/persistence/repository/functions/ProcedureOracleRepository.java
@@ -18,13 +18,15 @@ public class ProcedureOracleRepository {
 
     private final EntityManager em;
 
-    public BigInteger getCabeceraIdByProcedure(Long empresa, Long tipoDoc, Long almacen, Long codCliPro, Long usuario) {
+    public BigInteger getCabeceraIdByProcedure(Long empresa, Long tipoDoc, Long almacen, Long pventa, Long sigla, Long codCliPro, Long usuario) {
         try {
             StoredProcedureQuery query = em.createStoredProcedureQuery("PRG_USR.AST_WEB.CREAR_CABECERA_CCO");
 
             query.registerStoredProcedureParameter("PN_EMPRESA", Long.class, ParameterMode.IN);
             query.registerStoredProcedureParameter("PN_TIPODOC", Long.class, ParameterMode.IN);
             query.registerStoredProcedureParameter("PN_ALMACEN", Long.class, ParameterMode.IN);
+            query.registerStoredProcedureParameter("PN_PVENTA", Long.class, ParameterMode.IN);
+            query.registerStoredProcedureParameter("PN_SIGLA", Long.class, ParameterMode.IN);
             query.registerStoredProcedureParameter("PN_CODCLIPRO", Long.class, ParameterMode.IN);
             query.registerStoredProcedureParameter("PN_USUARIO", Long.class, ParameterMode.IN);
             query.registerStoredProcedureParameter("PN_COMPROBANTE", BigInteger.class, ParameterMode.OUT);
@@ -32,6 +34,8 @@ public class ProcedureOracleRepository {
             query.setParameter("PN_EMPRESA", empresa);
             query.setParameter("PN_TIPODOC", tipoDoc);
             query.setParameter("PN_ALMACEN", almacen);
+            query.setParameter("PN_PVENTA", almacen);
+            query.setParameter("PN_SIGLA", almacen);
             query.setParameter("PN_CODCLIPRO", codCliPro);
             query.setParameter("PN_USUARIO", usuario);
 
