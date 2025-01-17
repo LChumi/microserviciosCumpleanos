eedc67913680c54aeba39b3a62d8172151d389d3
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-01-17 18:49:57 -0500

    fix:(changes in procedure )

--- microservice-assist/src/main/java/com/cumpleanos/assist/service/implementation/SolicitudImportacionServiceImpl.java
+++ microservice-assist/src/main/java/com/cumpleanos/assist/service/implementation/SolicitudImportacionServiceImpl.java
@@ -1,8 +1,10 @@
 package com.cumpleanos.assist.service.implementation;
 
 import com.cumpleanos.assist.persistence.repository.functions.ProcedureOracleRepository;
+import com.cumpleanos.assist.service.exception.ProcedureNotCompletedException;
 import com.cumpleanos.assist.service.interfaces.ISolicitudImportacionService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,7 @@ import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
+@Slf4j
 public class SolicitudImportacionServiceImpl implements ISolicitudImportacionService {
 
     private final ProcedureOracleRepository procedureRepository;
@@ -24,10 +27,25 @@ public class SolicitudImportacionServiceImpl implements ISolicitudImportacionSer
                                          Long codCliPro,
                                          Long usuario,
                                          LocalDate fecha,
-                                         Long modulo
-    ) {
-        return procedureRepository.getCabeceraIdByProcedure(empresa, tipoDoc, almacen, pventa, sigla, codCliPro, usuario, fecha, modulo);
+                                         Long modulo) {
+        // Validar parámetros
+        if (empresa == null || tipoDoc == null || almacen == null || pventa == null || sigla == null ||
+                codCliPro == null || usuario == null || fecha == null || modulo == null) {
+            throw new IllegalArgumentException("Todos los parámetros son obligatorios.");
+        }
+
+        // Llamar al repositorio para ejecutar el procedimiento
+        try {
+            return procedureRepository.getCabeceraIdByProcedure(
+                    empresa, tipoDoc, almacen, pventa, sigla, codCliPro, usuario, fecha, modulo
+            );
+        } catch (ProcedureNotCompletedException e) {
+            // Puedes capturar y registrar detalles adicionales si es necesario
+            log.error("Error al generar la cabecera SCI: {}", e.getMessage(), e);
+            throw e; // Re-lanzar la excepción para que suba al controlador
+        }
     }
+
 
     @Override
     public void insertarDetalle(String detalle) {
