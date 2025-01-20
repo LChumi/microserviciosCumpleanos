0221b7067dd0d7df97685da093968094f31d2e0b
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-01-20 18:22:16 -0500

    fix: changes in repository

--- microservice-assist/src/main/java/com/cumpleanos/assist/persistence/repository/functions/FunctionOracleRepository.java
+++ microservice-assist/src/main/java/com/cumpleanos/assist/persistence/repository/functions/FunctionOracleRepository.java
@@ -1,5 +1,7 @@
 package com.cumpleanos.assist.persistence.repository.functions;
 
+import com.cumpleanos.assist.persistence.entity.UsuarioFavoritos;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -7,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.math.BigInteger;
 
 @Repository
-public interface FunctionOracleRepository {
+public interface FunctionOracleRepository extends JpaRepository<UsuarioFavoritos,Long> {
 
     @Query(value = "SELECT PRG_USR.AST_GEN.NUMERO_COMPROBANTE(:empresa, :coo) FROM DUAL", nativeQuery = true)
     String getComprobante(@Param("empresa") Long empresa, @Param("coo") BigInteger coo);
