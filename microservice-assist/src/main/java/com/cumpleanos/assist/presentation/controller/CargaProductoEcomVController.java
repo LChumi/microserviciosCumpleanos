c72614bdc49285c800760e2bce87678d0d86bc88
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-03-22 12:43:59 -0500

    feat added controller to new services

---
+++ microservice-assist/src/main/java/com/cumpleanos/assist/presentation/controller/CargaProductoEcomVController.java
@@ -0,0 +1,25 @@
+package com.cumpleanos.assist.presentation.controller;
+
+import com.cumpleanos.assist.service.interfaces.ICargaProductoEcomVService;
+import com.cumpleanos.core.models.views.CargaProductoEcomV;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("producto-ecom")
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+public class CargaProductoEcomVController {
+
+    private final ICargaProductoEcomVService service;
+
+    @GetMapping("/producto/{id}")
+    public ResponseEntity<CargaProductoEcomV> getProducto(@PathVariable Long id) {
+        CargaProductoEcomV p = service.findByProducto(id);
+        return ResponseEntity.ok(p);
+    }
+}
