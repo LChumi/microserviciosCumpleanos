2e900408701bf904ba09a8047117c9d9ce1ccc6f
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-04-04 19:28:12 -0500

    feat:added service in feignClient into microservice scheduler

--- scheduler-service/src/main/java/com/cumpleanos/scheduler/service/http/IEcommerceClient.java
+++ scheduler-service/src/main/java/com/cumpleanos/scheduler/service/http/IEcommerceClient.java
@@ -1,5 +1,6 @@
 package com.cumpleanos.scheduler.service.http;
 
+import com.cumpleanos.core.models.dto.ProductEcomRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,4 +14,7 @@ public interface IEcommerceClient {
 
     @GetMapping("/ecommerce/woocommerce/orders-date/{d1}/{d2}")
     ResponseEntity<List<Map<String, Object>>> getOrdersDate(@PathVariable LocalDate d1, @PathVariable LocalDate d2);
+
+    @PutMapping("/ecommerce/woocommerce/products/{sku}")
+    ResponseEntity<Map<String,Object>> updateProduct(@PathVariable String sku, ProductEcomRequest request);
 }
