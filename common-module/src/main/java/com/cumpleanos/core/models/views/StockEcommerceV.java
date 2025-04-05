d2e75c21bee04a088ab45e356bf7a973f50f78e8
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-04-04 19:01:00 -0500

    feat: added new endpoint to ecommerce
    -delete unused controller and added lombok annotation in StockEcommerceV.class

--- common-module/src/main/java/com/cumpleanos/core/models/views/StockEcommerceV.java
+++ common-module/src/main/java/com/cumpleanos/core/models/views/StockEcommerceV.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 import org.hibernate.annotations.Immutable;
 
 import java.math.BigDecimal;
 
+@Data
 @Entity
 @Immutable
 @Table(name = "STOCK_ECOMMERCE_V")
--- microservice-assist/src/main/java/com/cumpleanos/assist/presentation/controller/EcommerceController.java
+++ microservice-assist/src/main/java/com/cumpleanos/assist/presentation/controller/EcommerceController.java
@@ -1,8 +1,11 @@
 package com.cumpleanos.assist.presentation.controller;
 
 import com.cumpleanos.assist.service.interfaces.ecommerce.IProductosEcommerceService;
+import com.cumpleanos.assist.service.interfaces.ecommerce.IStockEcommerceVService;
 import com.cumpleanos.core.models.dto.ServiceResponse;
+import com.cumpleanos.core.models.views.StockEcommerceV;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,22 +13,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequestMapping("assist")
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
-public class CargaProductoEcommerceController {
+public class EcommerceController {
 
-    private final IProductosEcommerceService service;
+    private final IProductosEcommerceService serviceProductsEcommerce;
+    private final IStockEcommerceVService serviceStockEcommerce;
 
     @GetMapping("/ecommerce/producto-save/{id}/{empresa}")
     public ResponseEntity<ServiceResponse> getProducto(@PathVariable Long id, @PathVariable Long empresa) {
-        ServiceResponse p = service.uploadProductEcommerce(id, empresa);
+        ServiceResponse p = serviceProductsEcommerce.uploadProductEcommerce(id, empresa);
         return ResponseEntity.ok(p);
     }
 
     @GetMapping("/ecommerce/product-update/{id}/{empresa}/{sku}")
     public ResponseEntity<ServiceResponse> getProductoUpdate(@PathVariable Long id, @PathVariable Long empresa, @PathVariable String sku) {
-        ServiceResponse response = service.updateProductEcommerce(id,sku,empresa);
+        ServiceResponse response = serviceProductsEcommerce.updateProductEcommerce(id,sku,empresa);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/stock-ecommerce/view")
+    public ResponseEntity<List<StockEcommerceV>> findAll() {
+        List<StockEcommerceV> lista = serviceStockEcommerce.findAll();
+        log.info(lista.toString());
+        return ResponseEntity.ok(lista);
     }
 }
