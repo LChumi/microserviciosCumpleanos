b4a87b4b2123e62af3dabecdf5c09fcf198a9494
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-04-08 16:07:28 -0500

    feat:added new common module
    Core module with reusable records ,DTO and builder for cross-microservice use

---
+++ utility-classes-module/src/test/java/com/cumpleanos/common/UtilityClassesModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cumpleanos.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityClassesModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
