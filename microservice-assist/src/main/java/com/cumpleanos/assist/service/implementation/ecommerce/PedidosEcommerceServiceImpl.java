73fae373f4fcd86b019daa809eb413cc72bb4103
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-04-12 12:56:11 -0500

    feat:added new service

--- microservice-assist/src/main/java/com/cumpleanos/assist/service/implementation/ecommerce/PedidosEcommerceServiceImpl.java
+++ microservice-assist/src/main/java/com/cumpleanos/assist/service/implementation/ecommerce/PedidosEcommerceServiceImpl.java
@@ -1,4 +1,32 @@
 package com.cumpleanos.assist.service.implementation.ecommerce;
 
-public class PedidosEcommerceServiceImpl {
+import com.cumpleanos.assist.service.implementation.ClientServiceImpl;
+import com.cumpleanos.assist.service.interfaces.ecommerce.IPedidosEcommerceService;
+import com.cumpleanos.common.builders.ecommerce.PedidoWoocommerce;
+import com.cumpleanos.common.records.ServiceResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+public class PedidosEcommerceServiceImpl implements IPedidosEcommerceService {
+
+    private final EcommerceClientServiceImpl ecommerceClient;
+    private final ClientServiceImpl clienteService;
+
+    @Override
+    public ServiceResponse getPedidosAndUpdateSystem() {
+        LocalDate today = LocalDate.now();
+        LocalDate yesterday = today.minusDays(1);
+        List<PedidoWoocommerce> pedidosWoo = ecommerceClient.getOrdesrByDate(today, yesterday);
+        return null;
+    }
+
+    private void createCpedido(){
+
+    }
 }
---
+++ microservice-assist/src/main/java/com/cumpleanos/assist/service/interfaces/ecommerce/IPedidosEcommerceService.java
@@ -0,0 +1,8 @@
+package com.cumpleanos.assist.service.interfaces.ecommerce;
+
+import com.cumpleanos.common.records.ServiceResponse;
+
+public interface IPedidosEcommerceService {
+
+    ServiceResponse getPedidosAndUpdateSystem();
+}
