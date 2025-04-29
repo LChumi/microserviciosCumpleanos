8d4734dc79216fb493e4ea6f192fd75b4d8fc929
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-04-29 16:06:04 -0500

    fix: changes in method to resize Image ando conserve transparency

--- mongo-service/src/main/java/com/cumpleanos/mongo/utils/ImageUtils.java
+++ mongo-service/src/main/java/com/cumpleanos/mongo/utils/ImageUtils.java
@@ -6,12 +6,15 @@ import java.awt.image.BufferedImage;
 public class ImageUtils {
 
     public static BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) {
-        Image resultingImage = originalImage.getScaledInstance(targetWidth, targetHeight, Image.SCALE_SMOOTH);
-        BufferedImage outputImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_ARGB);
+        int type = originalImage.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
+        BufferedImage outputImage = new BufferedImage(targetWidth, targetHeight, type);
+
         Graphics2D g2d = outputImage.createGraphics();
-        g2d.drawImage(resultingImage, 0, 0, null);
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2d.setComposite(AlphaComposite.Src); // Soporte de transparencia
+        g2d.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
         g2d.dispose();
+
         return outputImage;
     }
-
 }
