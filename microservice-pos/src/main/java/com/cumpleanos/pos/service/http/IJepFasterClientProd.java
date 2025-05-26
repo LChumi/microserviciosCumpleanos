afb13ccc3e9e84d486ec6148b777c11b616caec4
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-05-26 12:07:48 -0500

    fix: changes in environments

--- microservice-pos/src/main/java/com/cumpleanos/pos/service/http/IJepFasterClientProd.java
+++ microservice-pos/src/main/java/com/cumpleanos/pos/service/http/IJepFasterClientProd.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "jepFfaster", url = "http://157.100.17.30:8285/serviciosenlineaBaseETH/")
+@FeignClient(name = "jepFfaster", url = "http://157.100.17.30:8285/serviciosenlineaBasev9/")
 public interface IJepFasterClientProd {
 
     @PostMapping("/integracioncomercial/qr-generation-process")
