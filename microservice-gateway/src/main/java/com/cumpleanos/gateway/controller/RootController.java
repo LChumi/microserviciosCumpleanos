40b568f8380fc2443adb811c975c32938aaedc49
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-07-11 16:09:52 -0500

    feat:fix yml and added robots.txt a disallow user agents

--- microservice-gateway/src/main/java/com/cumpleanos/gateway/controller/RootController.java
+++ microservice-gateway/src/main/java/com/cumpleanos/gateway/controller/RootController.java
@@ -1,10 +1,17 @@
 package com.cumpleanos.gateway.controller;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 public class RootController {
@@ -16,6 +23,16 @@ public class RootController {
                 exchange.getRequest().getURI().resolve("/actuator/info")
         );
         return exchange.getResponse().setComplete();
+    }
+
+    @GetMapping(value = "/robots.txt", produces = MediaType.TEXT_PLAIN_VALUE)
+    public Mono<ResponseEntity<Resource>> getRobotsFile() {
+        Resource resource = new ClassPathResource("static/robots.txt");
+        return Mono.just(ResponseEntity
+                .ok()
+                .cacheControl(CacheControl.maxAge(Duration.ofDays(30)).cachePublic())
+                .body(resource)
+        );
     }
 
 }
