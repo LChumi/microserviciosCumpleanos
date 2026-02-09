1e945cefe66ea3326479836c22daebb8e7e1fca7
LChumi <luischumi.9@gmail.com>
2026-02-09 10:59:19 -0500

    update sevrice to send main and sync ordes by worpdress

--- emails-service/src/main/java/com/cumpleanos/notification/configuration/EmailConfig.java
+++ emails-service/src/main/java/com/cumpleanos/notification/configuration/EmailConfig.java
@@ -2,22 +2,24 @@ package com.cumpleanos.notification.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
-//@Configuration
-//@PropertySource("classpath:email.properties")
+@Configuration
+@PropertySource("classpath:email.properties")
 public class EmailConfig {
 
-    //@Value("${email.sender}")
+    @Value("${email.sender}")
     private String emailUser;
 
-    //@Value("${email.password}")
+    @Value("${email.password}")
     private String emailPassword;
 
-    //@Bean
+    @Bean
     public JavaMailSender getMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
@@ -30,10 +32,12 @@ public class EmailConfig {
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
         props.put("mail.smtp.connectiontimeout", "10000");
         props.put("mail.smtp.timeout", "10000");
+        props.put("mail.smtp.writetimeout", "10000");
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        props.put("mail.debug", "true");
+
         return mailSender;
     }
 }
