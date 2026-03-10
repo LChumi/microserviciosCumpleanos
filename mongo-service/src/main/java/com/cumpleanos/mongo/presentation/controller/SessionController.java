402bdd60be465a9cd38207e0240bb1bef6e0398c
LChumi <luischumi.9@gmail.com>
2026-03-10 15:38:17 -0500

    feat: added services to get las logins session

--- mongo-service/src/main/java/com/cumpleanos/mongo/presentation/controller/SessionController.java
+++ mongo-service/src/main/java/com/cumpleanos/mongo/presentation/controller/SessionController.java
@@ -1,11 +1,15 @@
 package com.cumpleanos.mongo.presentation.controller;
 
+import com.cumpleanos.common.records.ServiceResponse;
+import com.cumpleanos.common.records.SessionDTO;
 import com.cumpleanos.mongo.service.interfaces.ISessionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("mongo")
@@ -14,5 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class SessionController {
 
     private final ISessionService sessionService;
+
+    @Operation(summary = "Guardar sesion", description = "Guardar la sesion de un Usuario")
+    @PostMapping("session/saved")
+    public ResponseEntity<ServiceResponse> saveSession(@RequestBody SessionDTO sessionDTO) {
+        ServiceResponse response = sessionService.saveSession(sessionDTO);
+        return ResponseEntity.ok(response);
+    }
+
+    @Operation(summary = "Ultima sesion", description = "Obtiene la ultima sesion guardada")
+    @Parameter(name = "userId", description = "Codigo de usuario")
+    @GetMapping("seesion/last-login/{userId}")
+    public ResponseEntity<ServiceResponse> getLastLogin(@PathVariable String userId) {
+        ServiceResponse response = sessionService.getLastLogin(userId);
+        return ResponseEntity.ok(response);
+    }
 
 }
