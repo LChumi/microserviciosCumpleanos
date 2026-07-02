05a9408a53bbf3a3c7cd91384dd53560cc0e83ab
LChumi <luischumi.9@gmail.com>
2026-07-02 09:27:38 -0500

    feat: added service to update canApr

--- assist-service/src/main/java/com/cumpleanos/assist/service/implementation/inventario/ProductoDespachoService.java
+++ assist-service/src/main/java/com/cumpleanos/assist/service/implementation/inventario/ProductoDespachoService.java
@@ -1,10 +1,13 @@
 package com.cumpleanos.assist.service.implementation.inventario;
 
 import com.cumpleanos.assist.persistence.repository.views.ProductoDespachoRepository;
+import com.cumpleanos.assist.service.implementation.ClientServiceImpl;
+import com.cumpleanos.common.records.ServiceResponse;
 import com.cumpleanos.core.models.views.FacDesprodWebV;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.List;
 
@@ -13,6 +16,7 @@ import java.util.List;
 public class ProductoDespachoService {
 
     private final ProductoDespachoRepository repository;
+    private final ClientServiceImpl clientService;
 
     public List<FacDesprodWebV> getProductosDespacho(Long empresa, BigInteger ccoCodigo, Long hoja){
         if (hoja == null) {
@@ -20,6 +24,26 @@ public class ProductoDespachoService {
         } else {
             return repository.findByEmpresaAndCcoCodigoAndHoja(empresa, ccoCodigo, hoja);
         }
+    }
+
+    public ServiceResponse actualizarCantidad(FacDesprodWebV p){
+
+        if (p.getCanapr().compareTo(BigDecimal.ZERO) >= 0) {
+            if (p.getCanapr().compareTo(BigDecimal.valueOf(p.getStock())) > 0) {
+                return new ServiceResponse("Cantidad no puede ser mayor al stock", false);
+            } else {
+                ServiceResponse response = null;
+                if (p.getTipodoc() == 26){
+                    response = clientService.addedCanAprDespacho(p.getCcoCodigo(),p.getProCodigo(),p.getCanapr().intValue());
+                } else if (p.getTipodoc() == 113){
+
+                }
+                return response;
+            }
+        } else{
+            return new ServiceResponse("Cantidad debe ser mayor o igual a 0", false);
+        }
+
     }
 
 }
