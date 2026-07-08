ed20dc69dceb41d1ed9c83f10ebd052242fe8920
LChumi <luischumi.9@gmail.com>
2026-07-08 10:50:19 -0500

    refactor: changes into requestmapping

--- meta-service/src/main/java/com/cumpleanos/meta/presentation/controller/TelegramNotificationController.java
+++ meta-service/src/main/java/com/cumpleanos/meta/presentation/controller/TelegramNotificationController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/telegram")
+@RequestMapping("/meta/telegram")
 @RequiredArgsConstructor
 public class TelegramNotificationController {
 
