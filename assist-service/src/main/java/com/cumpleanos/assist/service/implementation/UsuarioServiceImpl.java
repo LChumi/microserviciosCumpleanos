3e98742a76c449446d80304f93d5484ece99dfc3
LChumi <luischumi.9@gmail.com>
2026-08-11 16:38:44 -0500

    feta: added header to get ip by Cloudflare and nginx

--- assist-service/src/main/java/com/cumpleanos/assist/service/implementation/UsuarioServiceImpl.java
+++ assist-service/src/main/java/com/cumpleanos/assist/service/implementation/UsuarioServiceImpl.java
@@ -87,15 +87,20 @@ public class UsuarioServiceImpl {
         }
     }
 
-    private String getClientIp(HttpServletRequest request){
-        String ip = request.getHeader("X-Forwarded-For");
-        if (ip == null || ip.isEmpty()){
-            ip = request.getHeader("X-Real-IP");
+    private String getClientIp(HttpServletRequest request) {
+        String ip = request.getHeader("CF-Connecting-IP");
+        if (ip != null && !ip.isEmpty()) {
+            return ip;
         }
-        if (ip == null || ip.isEmpty()){
-            ip = request.getRemoteAddr();
+        ip = request.getHeader("X-Forwarded-For");
+        if (ip != null && !ip.isEmpty()) {
+            return ip.split(",")[0].trim();
         }
-        return ip;
+        ip = request.getHeader("X-Real-IP");
+        if (ip != null && !ip.isEmpty()) {
+            return ip;
+        }
+        return request.getRemoteAddr();
     }
 
 }
